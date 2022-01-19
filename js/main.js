025d229a9486e35532e261a60fb4554b4c7c81a8
Ronaldo Maciel <ronaldomacielcamposjunior@gmail.com>
2022-01-19 20:25:03 -0300

    add container-card

---
+++ js/main.js
@@ -0,0 +1,42 @@
+// window.onload = function () {
+//     let thor = document.getElementById('thor')
+//     let loki = document.getElementById('loki')
+//     let odin = document.getElementById('odin')
+//     let containerCard = document.getElementById('container-card')
+//     let fora = document.getElementById('container-fora')
+//     let popUp = document.getElementById('pop-up')
+
+//     fora.addEventListener('click', () => {
+//         if(popUp.classList.contains('display-block')){
+//             popUp.classList.add('display-none')
+//             popUp.classList.remove('display-block')
+//             containerCard.classList.add('display-grid')
+//             containerCard.classList.remove('display-block')
+            
+//         }
+//     })
+
+//     thor.addEventListener('click', () => {
+//         if(!containerCard.classList.contains('display-none')){
+//             containerCard.classList.remove('display-grid')
+//             containerCard.classList.add('display-none')
+//             popUp.classList.remove('display-none')
+//             popUp.classList.add('display-block')
+//             return
+//         }
+//     })
+//     odin.addEventListener('click', () => {
+//         if(!containerCard.classList.contains('display-none')){
+//             containerCard.classList.add('display-none')
+//             popUp.classList.add('display-block')
+//             return
+//         }
+//     })
+//     loki.addEventListener('click', () => {
+//         if(!containerCard.classList.contains('display-none')){
+//             containerCard.classList.add('display-none')
+//             popUp.classList.add('display-block')
+//             return
+//         }
+//     })
+// };
